26244e08a119fc68463856120a746f0211561ba1
nozomi0966 <nozomotoitech@gmail.com>
2018-03-23 00:37:35 +0900

    go fmt

--- main.go
+++ main.go
@@ -5,31 +5,31 @@ import (
 	"net"
 )
 
-func main(){
+func main() {
 	url := "localhost"
 	port := "8888"
 
-	listener, err := net.Listen("tcp", url + ":" + port)
-	if err != nil{
+	listener, err := net.Listen("tcp", url+":"+port)
+	if err != nil {
 		panic(err)
 	}
 
 	for {
 		conn, err := listener.Accept()
-		if err != nil{
+		if err != nil {
 			panic(err)
 		}
-		go func(){
+		go func() {
 			defer conn.Close()
 			echo(conn)
 		}()
 	}
 }
-func echo(conn net.Conn){
+func echo(conn net.Conn) {
 	buf := make([]byte, 256)
 	for {
 		n, err := conn.Read(buf)
-		if err != nil{
+		if err != nil {
 			if err == io.EOF {
 				break
 			}
