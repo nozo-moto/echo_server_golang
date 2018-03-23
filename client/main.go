e70a52cef605813a77c5f13a305566f0db612385
nozomi0966 <nozomotoitech@gmail.com>
2018-03-23 18:14:47 +0900

    make client

--- client/main.go
+++ client/main.go
@@ -1,5 +1,40 @@
 package main
 
 import (
-	
+	"net"
+	"os"
+	"bufio"
+	"log"
 )
+
+func main(){
+	url := "localhost"
+	port := "8888"
+	conn, err := net.Dial("tcp", url + ":" + port)
+	if err != nil{
+		panic(err)
+	}
+	defer conn.Close()
+
+	log.Println("Connected to server")
+	stream := bufio.NewScanner(os.Stdin)
+	for stream.Scan() {
+		if stream.Err() != nil {
+			log.Println(stream.Err())
+			break
+		}
+		text := stream.Text()
+		_, err := conn.Write([]byte(text))
+		if err != nil {
+			panic(err)
+		}
+		messageBuf := make([]byte, 256)
+		n, err := conn.Read(messageBuf)
+		if err != nil{
+			panic(err)
+		}
+		log.Println("Server : ", string(messageBuf[:n]))
+	}
+}
+
+
